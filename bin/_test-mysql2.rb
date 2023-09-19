3726c094bc8332aa754c874b9b74487fd8de253f
Denis Sablic <denis@readcube.com>
2023-09-19 11:10:31 +0200

    Temporarily disable ssl mode in test

--- bin/_test-mysql2.rb
+++ bin/_test-mysql2.rb
@@ -2,7 +2,7 @@ require 'mysql2'
 
 EXPECTED = [{"one"=>1}]
 
-client = Mysql2::Client.new host: "mysql", username: "root", password: "root"
+client = Mysql2::Client.new host: "mysql", username: "root", password: "root", ssl_mode: :disabled
 result = client.query("SELECT 1 AS one").each
 
 if result == EXPECTED
